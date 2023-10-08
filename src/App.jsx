786451e03073f9bdf1988545f4c6893848ce979d
adrianMartinezTeba <adrymate@gmail.com>
2023-10-08 15:32:48 +0200

    empezando el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.scss'
+import Home from "./components/Home/Home";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
